80be79fb26c16e9c793c69a0dac6107ecd0965b5
theinhtetaung-dev <theinhtetaung.developer@gmail.com>
2026-04-18 13:50:36 +0630

    feat: .select

--- THADotNetTrainingBatch1-LINQ-3/Program.cs
+++ THADotNetTrainingBatch1-LINQ-3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using THADotNetTrainingBatch1_LINQ_Data;
+using static THADotNetTrainingBatch1_LINQ_Data.LINQ3;
+
+var data = LINQ3.GetProducts();
+
+Console.Write("1:Electronic \n 2:Furniture \nEnter Option :   ");
+var inputOption = Console.ReadLine();
+bool isIntOption = int.TryParse(inputOption,out int opt);
+if(!isIntOption)
+{
+    Console.WriteLine("Invalid Integer!");
+}
+
+string Searching = opt == 1 ? "Electronics" : "Furniture";
+var searchItems = data.Where( x => x.Category == Searching)
+    .Select( x => new  {x.Name , x.Price}).ToList();
+
+foreach(var item in searchItems)
+{
+    Console.WriteLine("Name : " + item.Name + " , Price : " + item.Price);
+}
