627b9e8847ecfbcfbbb499227f220c1a028e834d
theinhtetaung-dev <theinhtetaung.developer@gmail.com>
2026-04-26 17:03:53 +0630

    docs: practice LINQ GroupBy and nested selection

---
+++ THADotNetTrainingBatch1-LINQ-4/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using THADotNetTrainingBatch1_LINQ_Data;
+
+var products = LINQ4.GetProducts();
+
+var onlyName = products.Select(x => x.Name).ToList();
+foreach (var name in onlyName)
+{
+    Console.WriteLine(name);
+}   
+Console.WriteLine();
+var gpProducts = products.GroupBy(x => x.Category)
+                 .Select(x => new
+                 {
+                     Name = x.Key,
+                     Count = x.Count()
+                 });
+
+foreach (var item in gpProducts)
+{
+    Console.WriteLine($"{item.Name}: {item.Count} items");
+}
+
+var gpItems = products.GroupBy(x => x.Category)
+                .Select(x => new
+                {
+                    Category = x.Key,
+                    items = x.Select(x => x.Name).ToList()
+                });
+
+Console.WriteLine();
+foreach (var group in gpItems)
+{
+    Console.WriteLine($"--- {group.Category} ---");
+    foreach (var productName in group.items)
+    {
+        Console.WriteLine($"  > {productName}");
+    }
+}
+
+Console.ReadLine(); 
---
+++ THADotNetTrainingBatch1-LINQ-Data/LINQ4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THADotNetTrainingBatch1_LINQ_Data;
+
+public class LINQ4
+{
+    public class Product
+    {
+        public string Name { get; set; } = null!;
+        public string Category { get; set; } = null!;
+    }
+
+        public static List<Product> GetProducts()
+        {
+            return new List<Product> {
+                new Product { Name = "Espresso", Category = "Coffee" },
+                new Product { Name = "Latte", Category = "Coffee" },
+                new Product { Name = "Green Tea", Category = "Tea" },
+                new Product { Name = "Cake", Category = "Bakery" },
+                new Product { Name = "Croissant", Category = "Bakery" },
+                new Product { Name = "Brownie", Category = "Bakery" }
+            };
+        }
+
+
+}
