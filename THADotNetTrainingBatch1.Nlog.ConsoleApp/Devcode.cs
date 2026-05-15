319de307d8d3edf4c8fadc9768123e79e914bbb8
theinhtetaung-dev <theinhtetaung.developer@gmail.com>
2026-05-15 23:07:12 +0630

    feat: instant method Logging

---
+++ THADotNetTrainingBatch1.Nlog.ConsoleApp/Devcode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace THADotNetTrainingBatch1.Nlog.ConsoleApp;
+
+public static class Devcode
+{
+    public static bool IsNullOrEmptyDev(this string input)
+    {
+        if( string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input))
+        {
+            NLog.LogManager.GetCurrentClassLogger().Warn("Input string is null, empty, or whitespace.");
+            return true;
+        }
+        return false;
+    }
+}
--- THADotNetTrainingBatch1.Nlog.ConsoleApp/Program.cs
+++ THADotNetTrainingBatch1.Nlog.ConsoleApp/Program.cs
@@ -1,34 +1,64 @@
 ﻿using NLog;
+using THADotNetTrainingBatch1.Nlog.ConsoleApp;
 
 Console.WriteLine("Hello, World!");
-
 var _logger = LogManager.GetCurrentClassLogger();
 
-bool isnum = false;
-string inputNum1 = Console.ReadLine()!;
-isnum = int.TryParse(inputNum1, out int num1);
-
-if(!isnum)
+try
 {
-    _logger.Error("Invalid input for num1: {Input}", inputNum1);
-    Console.WriteLine("Please enter a valid integer for num1.");
-    return;
-};
-_logger.Info("User entered num1: {Num1}", num1);
 
-string inputNum2 = Console.ReadLine()!;
-isnum = int.TryParse(inputNum2, out int num2);
+    bool isnum = false;
+    Console.Write("Enter your name : ");
+    string username = Console.ReadLine()!;
+    if(username.IsNullOrEmptyDev())
+    {
+        Console.WriteLine("Input cannot be empty. Please enter a valid name.");
+    }
+    Console.Write("Enter the num 1 : ");
+    string inputNum1 = Console.ReadLine()!;
+    if (inputNum1.IsNullOrEmptyDev())
+    {
+        Console.WriteLine("Input cannot be empty. Please enter a valid integer for num1.");
+    }
+    isnum = int.TryParse(inputNum1, out int num1);
 
-if(!isnum)
-{
-    _logger.Error("Invalid input for num2: {Input}", inputNum2);
-    Console.WriteLine("Please enter a valid integer for num2.");
-    return;
+
+    if (!isnum)
+    {
+        _logger.Error("Invalid input for num1: {Input}", inputNum1);
+        Console.WriteLine("Please enter a valid integer for num1.");
+        return;
+    }
+    ;
+    _logger.Info("User entered num1: {Num1}", num1);
+
+    Console.Write("Enter the num 2 : ");
+    string inputNum2 = Console.ReadLine()!;
+
+    if(inputNum2.IsNullOrEmptyDev())
+    {
+        Console.WriteLine("Input cannot be empty. Please enter a valid integer for num2.");
+    }
+
+    isnum = int.TryParse(inputNum2, out int num2);
+
+    if (!isnum)
+    {
+        _logger.Error("Invalid input for num2: {Input}", inputNum2);
+        Console.WriteLine("Please enter a valid integer for num2.");
+        return;
+    }
+    _logger.Info("User entered num2: {Num2}", num2);
+
+    int result = num1 + num2;
+    Console.WriteLine("Result: " + result);
+    _logger.Info("Calculated result of {Num1} + {Num2} = {Result}", num1, num2, result);
+
+    Console.ReadLine();
+
 }
-_logger.Info("User entered num2: {Num2}", num2);
+catch (Exception ex)
+{
+    _logger.Fatal(ex.Message);
 
-int result = num1 + num2;
-Console.WriteLine("Result: " + result); 
-_logger.Info("Calculated result of {Num1} + {Num2} = {Result}", num1, num2, result);
-
-Console.ReadLine();
+}
